c2cc321070720f86748284d3a369008441fd2e67
Sean Kempf <poserposer@me.com>
2020-09-01 21:47:29 -0700

    Created an object for each question

---
+++ script.js
@@ -0,0 +1,37 @@
+const questions = [
+    {
+        questionOne: "",
+        correctAnswer: "",
+        wrongAnswers: [
+            "", "", ""
+        ]
+    },
+    {
+        questionTwo: "",
+        correctAnswer: "",
+        wrongAnswers: [
+            "", "", ""
+        ]
+    },
+    {
+        questionThree: "",
+        correctAnswer: "",
+        wrongAnswers: [
+            "", "", ""
+        ]
+    },
+    {
+        questionFour: "",
+        correctAnswer: "",
+        wrongAnswers: [
+            "", "", ""
+        ]
+    },
+    {
+        questionFive: "",
+        correctAnswer: "",
+        wrongAnswers: [
+            "", "", ""
+        ]
+    }
+];
