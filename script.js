e326acce4169c4011eeb950bde5ff42353fee3f8
Sean Kempf <poserposer@me.com>
2020-09-03 21:58:00 -0700

    Added buttons for the answers to be displayed in the HTML

--- script.js
+++ script.js
@@ -1,37 +1,91 @@
+// This is an array for each question so I can pull from it later
 const questions = [
     {
         questionOne: "",
         correctAnswer: "",
-        wrongAnswers: [
-            "", "", ""
+        allAnswers: [
+            "", "", "",""
         ]
     },
     {
         questionTwo: "",
         correctAnswer: "",
-        wrongAnswers: [
-            "", "", ""
+        allAnswers: [
+            "", "", "",""
         ]
     },
     {
         questionThree: "",
         correctAnswer: "",
-        wrongAnswers: [
-            "", "", ""
+        allAnswers: [
+            "", "", "",""
         ]
     },
     {
         questionFour: "",
         correctAnswer: "",
-        wrongAnswers: [
-            "", "", ""
+        allAnswers: [
+            "", "", "",""
         ]
     },
     {
         questionFive: "",
         correctAnswer: "",
-        wrongAnswers: [
-            "", "", ""
+        allAnswers: [
+            "", "", "",""
         ]
     }
 ];
+
+// Declaring the timer constants
+const timeEl = document.querySelector("#time");
+
+let secondsLeft = 151;
+
+// Declaring what the each question and answers should be displayed
+let currentQuestion = 0;
+let questionOn = document.querySelector('#question');
+let allAnswerOne = document.querySelector('#answer1');
+let allAnswerTwo = document.querySelector('#answer2');
+let allAnswerThree = document.querySelector('#answer3');
+let allAnswerFour = document.querySelector('#answer4');
+let allAnswerFive = document.querySelector('#answer5');
+
+// What will happen when the Quiz starts???????
+// function beginQuiz(){
+
+
+
+function renderCurrentQuestion(){
+    for ()
+}
+// }
+// document.addEventListener("click", function (event) {
+
+//     if (event.target.matches("button")) {
+//         if (event.target.textContent === questions[currentQuestion].correctAnswer) {
+
+//         }
+//         else {
+
+//         };
+
+//         currentQuestion++;
+//     }
+// })
+
+
+
+
+function setTime() {
+  let timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+
+  }, 1000);
+}
+setTime();
