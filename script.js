5fe7c6b1d35c1a9476b99db5a9b1d708b85f3ab9
Sean Kempf <poserposer@me.com>
2020-09-06 20:39:35 -0700

    Created a proper start page for the quiz with a button that is declared in the script

--- script.js
+++ script.js
@@ -4,35 +4,35 @@ const questions = [
         questionOne: "What does CSS stand for?",
         correctAnswer: "Cascading Style Sheet",
         allAnswers: [
-            "Cascading Style Sheet", "Corisant Super Speedway", "Cyan Style Sheet","Computer Super Soldier"
+            "Cascading Style Sheet", "Corisant Super Speedway", "Cyan Style Sheet", "Computer Super Soldier"
         ]
     },
     {
         questionTwo: "What isn't a primitive date type in Javascript?",
         correctAnswer: "Array",
         allAnswers: [
-            "Strings", "Arrays", "Numbers","Booleans"
+            "Strings", "Arrays", "Numbers", "Booleans"
         ]
     },
     {
         questionThree: "What is the method best used for debugging in Javascript?",
         correctAnswer: "console.log",
         allAnswers: [
-            "function", "inspect", "console.log","camel-casing"
+            "function", "inspect", "console.log", "camel-casing"
         ]
     },
     {
         questionFour: "What does DOM stand for?",
         correctAnswer: "Document Object Model",
         allAnswers: [
-            "Document Object Model", "Data Object Material", "Dates On Monday","DOM"
+            "Document Object Model", "Data Object Material", "Dates On Monday", "DOM"
         ]
     },
     {
         questionFive: "What denotes something as a string in Javascript?",
         correctAnswer: "Quotations",
         allAnswers: [
-            "Brackets", "Quotations", "Angle Brackets","Semi-Colons"
+            "Brackets", "Quotations", "Angle Brackets", "Semi-Colons"
         ]
     }
 ];
@@ -49,18 +49,42 @@ let allAnswerOne = document.querySelector('#answer1');
 let allAnswerTwo = document.querySelector('#answer2');
 let allAnswerThree = document.querySelector('#answer3');
 let allAnswerFour = document.querySelector('#answer4');
-let allAnswerFive = document.querySelector('#answer5');
+let beginQuizButton = document.querySelector('#begin-quiz')
 
 // What will happen when the Quiz starts???????
 // function beginQuiz(){
 
+beginQuizButton.addEventListener("click", function beginQuiz(){
+});
+function beginQuiz(){
 
 
-function renderCurrentQuestion(){
-    for (let i = 0; i < questions.length; i++){
-
+    for (let i = 0; i < questions.length; i++) {
+        renderCurrentQuestion(questions[i]);
     }
+
+
+
+    function setTime() {
+        let timerInterval = setInterval(function () {
+            secondsLeft--;
+            timeEl.textContent = secondsLeft;
+    
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                renderHighScores();
+            }
+    
+        }, 1000);
+    }
+    setTime();
+function renderCurrentQuestion() {
+
+
+    
 }
+
+
 // }
 // document.addEventListener("click", function (event) {
 
@@ -78,16 +102,3 @@ function renderCurrentQuestion(){
 
 
 
-
-function setTime() {
-  let timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-    }
-
-  }, 1000);
-}
-setTime();
