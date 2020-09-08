267f0bf07dd93f7309a19d0a0c414853e7dde6da
Sean Kempf <poserposer@me.com>
2020-09-07 17:40:49 -0700

    Actually displaying the question when the start quiz button is pressed. Still need to add the choices as well as get it to move to the next question

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const questions = [
         question: "What does CSS stand for?",
         correctAnswer: "Cascading Style Sheet",
         allAnswers: [
-            "Cascading Style Sheet", "Corisant Super Speedway", "Cyan Style Sheet", "Computer Super Soldier"
+            "Cascading Style Sheet", "Coruscant Super Speedway", "Cyan Style Sheet", "Computer Super Soldier"
         ]
     },
     {
@@ -44,17 +44,17 @@ let secondsLeft = 150;
 
 // Declaring what the each question and answers should be displayed
 let currentQuestion = 0;
+let currentAnswer = 0;
 let questionOn = document.querySelector('#question');
-let answerOne = document.querySelector('#answer1');
-let answerTwo = document.querySelector('#answer2');
-let answerThree = document.querySelector('#answer3');
-let answerFour = document.querySelector('#answer4');
-let beginQuizButton = document.querySelector('#begin-quiz')
-let startingPage = document.querySelector('#starting-page')
-let questionPage = document.querySelector('#question-page')
+let answerBtn = document.querySelectorAll('.answer');
 
+let beginQuizButton = document.querySelector('#begin-quiz');
+let startingPage = document.querySelector('#starting-page');
+let questionPage = document.querySelector('#question-page');
+
+let correctDisplay = document.querySelector('#correct');
+let wrongDisplay = document.querySelector('#wrong');
 // What will happen when the Quiz starts???????
-
 
 
 
@@ -64,9 +64,30 @@ function beginQuiz() {
     beginQuizButton.classList.add('d-none');
     startingPage.classList.add('d-none');
     questionPage.classList.remove('d-none');
-   renderCurrentQuestion();
+    renderCurrentQuestion();
 
+    function renderCurrentQuestion() {
 
+        let currentQuestionOn = questions[currentQuestion].question
+        questionOn.textContent = currentQuestionOn;
+        renderCurrentAnswers();
+
+    };
+    function renderCurrentAnswers() {
+        for (let i = 0; i < questions[i].allAnswers; i++) {
+            let currentAnswers = questions[i].allAnswers[i];
+            answer.textContent = currentAnswers;
+
+        }
+    }
+    answerBtn.addEventListener('click', function () {
+        if (answer.textContent === questions[i].correctAnswer) {
+            correctDisplay.classList.remove('d-none');
+        }
+        else {
+            wrongDisplay.classList.remove('d-none');
+        };
+    })
     function setTime() {
         let timerInterval = setInterval(function () {
             secondsLeft--;
@@ -79,7 +100,7 @@ function beginQuiz() {
 
         }, 1000);
     }
-    // renderCurrentQuestion();
+
     setTime();
 }
 
@@ -91,45 +112,3 @@ beginQuizButton.addEventListener("click", beginQuiz)
 
 
 
-  // function renderCurrentQuestion() {
-    //     for (let i = 0; i < questions.length; i++) {
-    //         if (i === 0) {
-    //             questionOn.textContent = questions.questionOne;
-    //             answerOne.textContent = questions.allAnswersOne[0];
-    //             answerTwo.textContent = questions.allAnswersOne[1];
-    //             answerThree.textContent = questions.allAnswersOne[2];
-    //             answerFour.textContent = questions.allAnswersOne[3];
-
-    //         }
-    //         else if (i === 1) {
-    //             questionOn.textContent = questions.questionTwo;
-    //             answerOne.textContent = questions.allAnswersTwo[0];
-    //             answerTwo.textContent = questions.allAnswersTwo[1];
-    //             answerThree.textContent = questions.allAnswersTwo[2];
-    //             answerFour.textContent = questions.allAnswersTwo[3];
-    //         }
-    //         else if (i === 2) {
-    //             questionOn.textContent = questions.questionThree;
-    //             answerOne.textContent = questions.allAnswersThree[0];
-    //             answerTwo.textContent = questions.allAnswersThree[1];
-    //             answerThree.textContent = questions.allAnswersThree[2];
-    //             answerFour.textContent = questions.allAnswersThree[3];
-
-    //         }
-    //         else if (i === 3) {
-    //             questionOn.textContent = questions.questionFour;
-    //             answerOne.textContent = questions.allAnswersFour[0];
-    //             answerTwo.textContent = questions.allAnswersFour[1];
-    //             answerThree.textContent = questions.allAnswersFour[2];
-    //             answerFour.textContent = questions.allAnswersFour[3];
-
-    //         }
-    //         else if (i === 4) {
-    //             questionOn.textContent = questions.questionFive;
-    //             answerOne.textContent = questions.allAnswersFive[0];
-    //             answerTwo.textContent = questions.allAnswersFive[1];
-    //             answerThree.textContent = questions.allAnswersFive[2];
-    //             answerFour.textContent = questions.allAnswersFive[3];
-
-    //         }
-    //     }
