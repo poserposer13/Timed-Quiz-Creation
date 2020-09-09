edebb9e9ad53616a8dedb9b2606eeb9201a967cd
Sean Kempf <poserposer@me.com>
2020-09-08 22:17:43 -0700

    Added html to have a high scores page

--- script.js
+++ script.js
@@ -57,18 +57,22 @@ let questionPage = document.querySelector('#question-page');
 
 let correctDisplay = document.querySelector('#correct');
 let wrongDisplay = document.querySelector('#wrong');
-// What will happen when the Quiz starts???????
+
+let highScorePage = document.querySelector('#high-score-page');
+let highScore = document.querySelector('#high-score');
+let highScoreBtn = document.querySelector('#score-button');
 
 
 
 
-
+// Clicking the Begin Quiz Button
 function beginQuiz() {
     beginQuizButton.classList.add('d-none');
     startingPage.classList.add('d-none');
     questionPage.classList.remove('d-none');
     renderCurrentQuestion();
 
+    // Puts a question on the screen for the user to answer
     function renderCurrentQuestion() {
 
         let currentQuestionOn = questions[currentQuestion].question
@@ -76,6 +80,7 @@ function beginQuiz() {
         renderCurrentAnswers();
 
     };
+    // Puts all the answers on the screen for the user to click on 
     function renderCurrentAnswers() {
         let currentAnswerOne = questions[currentQuestion].allAnswers[0];
         let currentAnswerTwo = questions[currentQuestion].allAnswers[1];
@@ -86,26 +91,60 @@ function beginQuiz() {
         answerThree.textContent = currentAnswerThree;
         answerFour.textContent = currentAnswerFour;
     }
-    // console.log(answerBtn)
+    // Determining if the button clicked was correct or not
     for (let i = 0; i < answerBtn.length; i++) {
         answerBtn[i].addEventListener('click', function () {
 
-            if (answerBtn.textContent === questions[currentQuestion].correctAnswer) {
+            if (answerBtn[i].textContent === questions[currentQuestion].correctAnswer) {
                 correctDisplay.classList.remove('d-none');
+                wrongDisplay.classList.add('d-none');
                 nextQuestion();
             }
             else {
                 wrongDisplay.classList.remove('d-none');
+                correctDisplay.classList.add('d-none');
                 secondsLeft -= 5;
                 nextQuestion();
             };
         })
     }
-
+    // Selecting the next question
     function nextQuestion() {
         currentQuestion++;
-        renderCurrentQuestion();
-    }
+        if (currentQuestion === questions.length)
+            renderHighScores();
+        else {
+            renderCurrentQuestion();
+        }
+    };
+
+    function renderHighScores() {
+        wrongDisplay.classList.add('d-none');
+        correctDisplay.classList.add('d-none');
+        questionPage.classList.add('d-none');
+        highScorePage.classList.remove('d-none');
+
+
+
+    };
+    function initialSave() {
+        
+        let initializer = highScore.value.trim();
+        if (initializer !== "") {
+            let highScore = JSON.parse(window.localStorage.getItem("high-score") || "[]");
+
+            let newScore = {
+                score: secondsLeft,
+                highScore: initializer,
+            }
+            highScore.push(newScore);
+            window.localStorage.setItem("high-score", JSON.stringify(highScore));
+            window.location.href = "index.html";
+        }
+    };
+    highScoreBtn.addEventListener('click', initialSave);
+
+    // Setting the Timer
     function setTime() {
         let timerInterval = setInterval(function () {
             secondsLeft--;
