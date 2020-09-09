762fc48e5e65a9554444c7874fb4c61fddff1b30
Sean Kempf <poserposer@me.com>
2020-09-08 21:36:30 -0700

    Added the answer to the buttons, functionality to the buttons, next question pulls up when an answer is given, timer goes down more if the answer is wrong

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // This is an array for each question so I can pull from it later
 const questions = [
     {
-        question: "What does CSS stand for?",
+        question: "1) What does CSS stand for?",
         correctAnswer: "Cascading Style Sheet",
         allAnswers: [
             "Cascading Style Sheet", "Coruscant Super Speedway", "Cyan Style Sheet", "Computer Super Soldier"
         ]
     },
     {
-        question: "What isn't a primitive date type in Javascript?",
-        correctAnswer: "Array",
+        question: "2) What isn't a primitive date type in Javascript?",
+        correctAnswer: "Arrays",
         allAnswers: [
             "Strings", "Arrays", "Numbers", "Booleans"
         ]
     },
     {
-        question: "What is the method best used for debugging in Javascript?",
+        question: "3) What is the method best used for debugging in Javascript?",
         correctAnswer: "console.log",
         allAnswers: [
             "append", "query selector", "console.log", "camel casing"
         ]
     },
     {
-        question: "What does DOM stand for?",
+        question: "4) What does DOM stand for?",
         correctAnswer: "Document Object Model",
         allAnswers: [
             "Document Object Model", "Data Object Material", "Dates On Monday", "DOM"
         ]
     },
     {
-        question: "What denotes something as a string in Javascript?",
+        question: "5) What denotes something as a string in Javascript?",
         correctAnswer: "Quotations",
         allAnswers: [
             "Brackets", "Quotations", "Angle Brackets", "Semi-Colons"
@@ -47,7 +47,10 @@ let currentQuestion = 0;
 let currentAnswer = 0;
 let questionOn = document.querySelector('#question');
 let answerBtn = document.querySelectorAll('.answer');
-
+let answerOne = document.querySelector('#answer1');
+let answerTwo = document.querySelector('#answer2');
+let answerThree = document.querySelector('#answer3');
+let answerFour = document.querySelector('#answer4');
 let beginQuizButton = document.querySelector('#begin-quiz');
 let startingPage = document.querySelector('#starting-page');
 let questionPage = document.querySelector('#question-page');
@@ -74,20 +77,35 @@ function beginQuiz() {
 
     };
     function renderCurrentAnswers() {
-        for (let i = 0; i < questions[i].allAnswers; i++) {
-            let currentAnswers = questions[i].allAnswers[i];
-            answer.textContent = currentAnswers;
-
-        }
+        let currentAnswerOne = questions[currentQuestion].allAnswers[0];
+        let currentAnswerTwo = questions[currentQuestion].allAnswers[1];
+        let currentAnswerThree = questions[currentQuestion].allAnswers[2];
+        let currentAnswerFour = questions[currentQuestion].allAnswers[3];
+        answerOne.textContent = currentAnswerOne;
+        answerTwo.textContent = currentAnswerTwo;
+        answerThree.textContent = currentAnswerThree;
+        answerFour.textContent = currentAnswerFour;
     }
-    answerBtn.addEventListener('click', function () {
-        if (answer.textContent === questions[i].correctAnswer) {
-            correctDisplay.classList.remove('d-none');
-        }
-        else {
-            wrongDisplay.classList.remove('d-none');
-        };
-    })
+    // console.log(answerBtn)
+    for (let i = 0; i < answerBtn.length; i++) {
+        answerBtn[i].addEventListener('click', function () {
+
+            if (answerBtn.textContent === questions[currentQuestion].correctAnswer) {
+                correctDisplay.classList.remove('d-none');
+                nextQuestion();
+            }
+            else {
+                wrongDisplay.classList.remove('d-none');
+                secondsLeft -= 5;
+                nextQuestion();
+            };
+        })
+    }
+
+    function nextQuestion() {
+        currentQuestion++;
+        renderCurrentQuestion();
+    }
     function setTime() {
         let timerInterval = setInterval(function () {
             secondsLeft--;
